5f3498cf5d084bb1eb3ee9166fffd8364239bd8a
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2022-03-10 19:35:48 +0900

    improvement feat parser for repl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rmonkey::repl;
 fn main() {
     println!("Hello! This is the Monkey programming language");
-    repl::start(std::io::stdin(), std::io::stdout());
+    repl::start(std::io::stdin(), std::io::stdout()).unwrap();
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,4 +1,5 @@
 use crate::lexer::*;
+use crate::parser::*;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::io::Read;
@@ -6,20 +7,21 @@ use std::io::{self, Write};
 
 static PROMPT: &str = ">> ";
 
-pub fn start<R, W>(src: R, out: W) -> std::io::Result<()>
+pub fn start<R, W>(src: R, mut out: W) -> std::io::Result<()>
 where
     R: Read,
     W: Write,
 {
     let mut reader = BufReader::new(src);
     loop {
-        print!("{}", PROMPT);
-        io::stdout().flush().unwrap();
+        out.write(PROMPT.as_bytes())?;
+        io::stdout().flush()?;
         let mut line = String::new();
         reader.read_line(&mut line)?;
         let l = Lexer::new(&line);
-        for tok in l {
-            println!("{:?}", tok);
-        }
+        let mut p = Parser::new(l);
+        let program = p.parse_program().unwrap();
+        out.write(program.to_string().as_bytes())?;
+        out.write(b"\n")?;
     }
 }
